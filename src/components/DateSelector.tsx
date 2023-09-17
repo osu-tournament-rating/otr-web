fa497d3eb460bb9d9b6805eb259cd78ba22c82af
hburn7 <hburnett777@gmail.com>
2023-09-17 13:16:30 -0400

    mobile-first dashboard

--- src/components/DateSelector.tsx
+++ src/components/DateSelector.tsx
@@ -8,13 +8,17 @@ export interface IDateSelectorProps {
 function DateSelector({ currentDays, setDays }: IDateSelectorProps) {
     return (
         <>
-            <div className="flex flex-row mx-10 space-x-5">
-                <DateSelectorButton currentDays={currentDays} days={30} setDays={setDays} label="30 days" />
-                <DateSelectorButton currentDays={currentDays} days={90} setDays={setDays} label="90 days" />
-                <DateSelectorButton currentDays={currentDays} days={180} setDays={setDays} label="6 months" />
-                <DateSelectorButton currentDays={currentDays} days={365} setDays={setDays} label="1 year" />
-                <DateSelectorButton currentDays={currentDays} days={730} setDays={setDays} label="2 years" />
-                <DateSelectorButton currentDays={currentDays} days={-1} setDays={setDays} label="All time" />
+            <div className="flex flex-col sm:flex-row mx-auto sm:mx-10 space-x-5">
+                <div className="flex space-x-5 my-2">
+                    <DateSelectorButton currentDays={currentDays} days={30} setDays={setDays} label="30 days" />
+                    <DateSelectorButton currentDays={currentDays} days={90} setDays={setDays} label="90 days" />
+                    <DateSelectorButton currentDays={currentDays} days={180} setDays={setDays} label="6 months" />
+                </div>
+                <div className="flex space-x-5 my-2">
+                    <DateSelectorButton currentDays={currentDays} days={365} setDays={setDays} label="1 year" />
+                    <DateSelectorButton currentDays={currentDays} days={730} setDays={setDays} label="2 years" />
+                    <DateSelectorButton currentDays={currentDays} days={-1} setDays={setDays} label="All time" />
+                </div>
             </div>
         </>
     );
