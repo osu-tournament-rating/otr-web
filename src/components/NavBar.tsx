508f7429d52f7bfd945a75e3c3224a1fbae2b7bc
hburn7 <hburnett777@gmail.com>
2023-09-17 01:41:56 -0400

    mobile-first navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,28 +1,46 @@
+import React, { useState } from 'react';
+
 function NavBar() {
+  const [isMobileNavVisible, setIsMobileNavVisible] = useState(false);
+
   return (
-    <div className="bg-gray-100 rounded-xl p-4 m-10 flex items-center justify-between">
-    <a href="/">
-        <img className="w-8 flex" src="logos/small.svg" alt="Company Logo" />
-    </a>
-    
-    <div className="flex space-x-8">
-        <a href="/Dashboard" className="mx-4 text-dark text-xl font-medium font-sans hover:text-gray-600">Dashboard</a>
-        <a href="/Leaderboards" className="mx-4 text-dark text-xl font-medium font-sans hover:text-gray-600">Leaderboards</a>
-        <a href="/Submit" className="mx-4 text-dark text-xl font-medium font-sans hover:text-gray-600 hover:drop-shadow-sm">Submit matches</a>
-    </div>
+    <div className="bg-gray-100 rounded-xl p-4 m-10">
+      <div className="flex items-center justify-between">
+        <a href="/">
+          <img className="w-8 flex" src="logos/small.svg" alt="Company Logo" />
+        </a>
+        
+        {/* Desktop Nav */}
+        <div className="hidden md:flex items-center justify-between w-full mx-10">
+          <div className="flex space-x-8">
+            <a href="/Dashboard" className="text-dark text-xl font-medium font-sans hover:text-gray-600">Dashboard</a>
+            <a href="/Leaderboards" className="text-dark text-xl font-medium font-sans hover:text-gray-600">Leaderboards</a>
+            <a href="/Submit" className="text-dark text-xl font-medium font-sans hover:text-gray-600 hover:drop-shadow-sm">Submit matches</a>
+          </div>
 
-    <a href="/Donate" className="mx-4 text-dark text-xl font-medium font-sans hover:text-gray-600 hover:drop-shadow-sm">Donate</a>
+          <div className="flex-2/3 justify-end">
+            <a href="/Donate" className="mx-10 text-dark text-xl font-medium font-sans hover:text-gray-600 hover:drop-shadow-sm">Donate</a>
+          </div>
+        </div>
 
-    <div className="flex space-x-4">
-        {/* <button className="text-gray-600 hover:text-gray-800">
-            🌙
+        {/* Hamburger Menu Icon for mobile */}
+        <button 
+          onClick={() => setIsMobileNavVisible(!isMobileNavVisible)}
+          className="md:hidden p-2 focus:outline-none focus:shadow-outline">
+          ☰
         </button>
-        <button className="text-gray-600 hover:text-gray-800">
-            👤
-        </button> */}
-    </div>
-</div>
+      </div>
 
+      {/* Mobile Nav */}
+      {isMobileNavVisible && (
+        <div className="md:hidden mt-4 w-full">
+          <a href="/Dashboard" className="block text-dark text-xl font-medium font-sans hover:text-gray-600 px-4 py-2">Dashboard</a>
+          <a href="/Leaderboards" className="block text-dark text-xl font-medium font-sans hover:text-gray-600 px-4 py-2">Leaderboards</a>
+          <a href="/Submit" className="block text-dark text-xl font-medium font-sans hover:text-gray-600 hover:drop-shadow-sm px-4 py-2">Submit matches</a>
+          <a href="/Donate" className="block text-dark text-xl font-medium font-sans hover:text-gray-600 hover:drop-shadow-sm px-4 py-2">Donate</a>
+        </div>
+      )}
+    </div>
   );
 }
 
