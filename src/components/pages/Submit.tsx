232e36630abeb586e29b52a4bdee03e3578dc45d
hburn7 <hburnett777@gmail.com>
2023-09-16 22:16:39 -0400

    grammar

--- src/components/pages/Submit.tsx
+++ src/components/pages/Submit.tsx
@@ -12,7 +12,7 @@ function Submit() {
           <SubmissionGuidelinesCard title="Guidelines" description="Please follow the posted guidelines before submitting your matches. Keep in mind that posting matches that don't follow the rules may lead to a restriction." />
           <SubmissionGuidelinesCard title="" description="Our goal is to accept matches that do not stray too far away from the 'traditional' tournament format (head-to-head & team vs). Group stages of well-run tournaments are allowed." />
           <SubmissionGuidelinesCard title="" description="Extreme gimmicks such as tag, relax, and battle-royale styled matches will not be included. Bracket-stage and group-stage matches only." />
-          <SubmissionGuidelinesCard title="" description="Qualifiers and warmups will not be included" />
+          <SubmissionGuidelinesCard title="" description="Qualifiers and warmups will not be included." />
           <SubmissionGuidelinesCard title="" description="The o!TR team reserves the right to make final judgements on whether a tournament or match should be deemed as fair and competitive play." />
         </div>
         <div className="flex-row w-2/5">
