4c71b00d6a683cc69d6c7054bc1b80eeb6ac8174
hburn7 <hburnett777@gmail.com>
2023-09-25 20:26:37 -0400

    Leaderboard styling

--- src/components/cards/LeaderboardTable.tsx
+++ src/components/cards/LeaderboardTable.tsx
@@ -4,33 +4,28 @@ interface ILeaderboardTableProps {
 
 function LeaderboardTable({ leaderboardData }: ILeaderboardTableProps) {
     return (
-        <div className="bg-gray-100 table w-full rounded-xl m-10 font-sans">
-            <div className="table-header-group">
-                <div className="table-row">
-                    <div className="table-cell font-bold">Rank</div>
-                    <div className="table-cell font-bold">Player</div>
-                    <div className="table-cell font-bold">Tier</div>
-                    <div className="table-cell font-bold">Rating</div>
-                    <div className="table-cell font-bold">Matches</div>
-                    <div className="table-cell font-bold">Winrate</div>
-                </div>
-            </div>
-            <div className="table-row-group font-sans">
-                {leaderboardData.map((player, index) => {
-                    return (
-                        <div className="table-row bg-gray-200 rounded-xl my-2" key={index}>
-                            <div className="table-cell">#{player.rank}</div>
-                            <div className="table-cell">{player.username}</div>
-                            <div className="table-cell">{player.tier}</div>
-                            <div className="table-cell">{player.rating}</div>
-                            <div className="table-cell">{player.Matches}</div>
-                            <div className="table-cell">{(player.Winrate * 100).toFixed(2)}%</div>
-                        </div>
-                    )
-                })}
-            </div>
+      <div className="bg-gray-100 w-full rounded-xl m-10 font-sans p-4">
+        <div className="flex mb-4 px-2 rounded-md">
+          <div className="flex-1 font-bold">Rank</div>
+          <div className="flex-1 font-bold">Player</div>
+          <div className="flex-1 font-bold">Tier</div>
+          <div className="flex-1 font-bold">Rating</div>
+          <div className="flex-1 font-bold">Matches</div>
+          <div className="flex-1 font-bold">Winrate</div>
         </div>
-    )
-}
+        {leaderboardData.map((player, index) => (
+          <div className="flex mb-4 bg-gray-200 rounded-xl p-4 -mx-5" key={index}>
+            <div className="flex-1 ml-5">#{player.rank}</div>
+            <div className="flex-1 -ml-5">{player.username}</div>
+            <div className="flex-1">{player.tier}</div>
+            <div className="flex-1">{player.rating}</div>
+            <div className="flex-1">{player.Matches}</div>
+            <div className="flex-1">{(player.Winrate * 100).toFixed(2)}%</div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+  
 
 export default LeaderboardTable;
