2256153ec9bdb077bab69d8ffef6e257db66ded7
Giovanni Martire <akinari@outlook.it>
2023-11-23 14:19:21 +0100

    first initialization of nextjs

--- src/components/cards/LandingCardLogo.tsx
+++
@@ -1,9 +0,0 @@
-function LandingCardLogo() {
-    return (
-        <div className="card bg-gray-100 rounded-xl p-10 m-5 md:m-10 flex md:w-64 justify-center items-center">
-            <img className="w-64 block" src="logos/full.svg" alt="" />
-        </div>
-    );
-}
-
-export default LandingCardLogo;
