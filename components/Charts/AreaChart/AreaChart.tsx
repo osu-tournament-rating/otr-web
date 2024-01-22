dea4ac4477b28b54e58a4a2d5177e565433b6ca7
Giovanni Martire <akinari@outlook.it>
2024-01-22 14:57:32 +0100

    Custom tooltip for Dashboard graph

--- components/Charts/AreaChart/AreaChart.tsx
+++ components/Charts/AreaChart/AreaChart.tsx
@@ -11,6 +11,7 @@ import {
   Title,
   Tooltip,
 } from 'chart.js';
+import clsx from 'clsx';
 import { useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import styles from './AreaChart.module.css';
@@ -26,7 +27,6 @@ ChartJS.register(
   Legend
 );
 
-/*
 const getOrCreateTooltip = (chart) => {
   let tooltipEl = chart.canvas.parentNode.querySelector('div');
 
@@ -34,100 +34,16 @@ const getOrCreateTooltip = (chart) => {
     tooltipEl = document.createElement('div');
     tooltipEl.className = styles.tooltip;
 
-    const table = document.createElement('table');
-    table.style.margin = '0px';
+    const div = document.createElement('div');
+    div.className = 'tooltip';
+    div.style.margin = '0px';
 
-    tooltipEl.appendChild(table);
+    tooltipEl.appendChild(div);
     chart.canvas.parentNode.appendChild(tooltipEl);
   }
 
   return tooltipEl;
 };
-
-export const externalTooltipHandler = (context) => {
-  // Tooltip Element
-  const { chart, tooltip } = context;
-  const tooltipEl = getOrCreateTooltip(chart);
-
-
-
-  // Hide if no tooltip
-  if (tooltip.opacity === 0) {
-    tooltipEl.style.opacity = 0;
-    return;
-  }
-
-  // Set Text
-  if (tooltip.body) {
-    const titleLines = tooltip.title || [];
-    const bodyLines = tooltip.body.map((b) => b.lines);
-
-    const tableHead = document.createElement('thead');
-
-    titleLines.forEach((title) => {
-      const tr = document.createElement('tr');
-      tr.style.borderWidth = 0;
-
-      const th = document.createElement('th');
-      th.style.borderWidth = 0;
-      const text = document.createTextNode(title);
-
-      th.appendChild(text);
-      tr.appendChild(th);
-      tableHead.appendChild(tr);
-    });
-
-    const tableBody = document.createElement('tbody');
-    bodyLines.forEach((body, i) => {
-      const colors = tooltip.labelColors[i];
-
-      const span = document.createElement('span');
-      span.style.background = colors.backgroundColor;
-      span.style.borderColor = colors.borderColor;
-      span.style.borderWidth = '2px';
-      span.style.marginRight = '10px';
-      span.style.height = '10px';
-      span.style.width = '10px';
-      span.style.display = 'inline-block';
-
-      const tr = document.createElement('tr');
-      tr.style.backgroundColor = 'inherit';
-      tr.style.borderWidth = 0;
-
-      const td = document.createElement('td');
-      td.style.borderWidth = 0;
-
-      const text = document.createTextNode(body);
-
-      td.appendChild(span);
-      td.appendChild(text);
-      tr.appendChild(td);
-      tableBody.appendChild(tr);
-    });
-
-    const tableRoot = tooltipEl.querySelector('table');
-
-    // Remove old children
-    while (tableRoot.firstChild) {
-      tableRoot.firstChild.remove();
-    }
-
-    // Add new children
-    tableRoot.appendChild(tableHead);
-    tableRoot.appendChild(tableBody);
-  }
-
-  const { offsetLeft: positionX, offsetTop: positionY } = chart.canvas;
-
-  // Display, position, and set styles for font
-  tooltipEl.style.opacity = 1;
-  tooltipEl.style.left = positionX + tooltip.caretX + 'px';
-  tooltipEl.style.top = positionY + tooltip.caretY + 'px';
-  tooltipEl.style.font = tooltip.options.bodyFont.string;
-  tooltipEl.style.padding =
-    tooltip.options.padding + 'px ' + tooltip.options.padding + 'px';
-};
- */
 
 export default function AreaChart({
   ratingStats,
@@ -143,7 +59,9 @@ export default function AreaChart({
   /* get variables of colors from CSS */
   useEffect(() => {
     setColors([
-      getComputedStyle(document.documentElement).getPropertyValue('--blue-600'),
+      getComputedStyle(document.documentElement).getPropertyValue(
+        '--accent-color'
+      ),
       getComputedStyle(document.documentElement).getPropertyValue('--blue-400'),
     ]);
     setFont(
@@ -153,8 +71,141 @@ export default function AreaChart({
     );
   }, []);
 
+  const dateFormatOptions = { year: 'numeric', month: 'short', day: 'numeric' };
+
+  let labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  let dataForGraph: number[] = labels.map(() => Math.ceil(Math.random() * 100));
+  let tournamentsTooltip: object[];
+
+  if (ratingStats) {
+    labels = ratingStats.map((match) =>
+      new Date(match.tooltipInfo.matchDate).toLocaleDateString(
+        'en-US',
+        dateFormatOptions
+      )
+    );
+    /* labels = Array.from(new Set(labels)); */
+
+    tournamentsTooltip = ratingStats.map((match) => {
+      /* console.log(match.tooltipInfo.matchDate); */
+      match.tooltipInfo.matchDate = new Date(
+        match.tooltipInfo.matchDate
+      ).toLocaleDateString('en-US', dateFormatOptions);
+
+      return match;
+    });
+
+    dataForGraph = ratingStats.map((match) => {
+      /* tournamentsTooltip.filter(
+        (m) => m.tooltipInfo.matchDate == tooltip.title
+      ); */
+
+      return match.ratingAfter.toFixed(0);
+    });
+  }
+
+  /* if (rankChart) {
+    labels = rankChart.map((match) => match.matchName);
+    dataForGraph = rankChart.map((match) => match.rank.toFixed(0));
+  } */
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        fill: true,
+        label: '',
+        data: dataForGraph,
+        borderWidth: 3,
+        borderColor: `hsla(${colors[0]}, 0.6)`,
+        backgroundColor: 'transparent' /* `hsla(${colors[1]}, 0.6)` */,
+        font: font,
+      },
+    ],
+  };
+
+  const externalTooltipHandler = (context) => {
+    // Tooltip Element
+    const { chart, tooltip } = context;
+    const tooltipEl = getOrCreateTooltip(chart);
+
+    /* console.log(tournamentsTooltip); */
+
+    // Hide if no tooltip
+    if (tooltip.opacity === 0) {
+      tooltipEl.style.opacity = 0;
+      return;
+    }
+
+    if (tooltip.body && ratingStats) {
+      const matchesLines = tournamentsTooltip.filter(
+        (match) => match.tooltipInfo.matchDate == tooltip.title
+      );
+
+      /* console.log(matchesLines); */
+
+      const matchesList = document.createElement('ul');
+      matchesLines.forEach((match, i) => {
+        console.log(match, i);
+
+        const li = document.createElement('li');
+
+        const matchName = document.createElement('a');
+        matchName.href = match.tooltipInfo.mpLink;
+        matchName.target = '_blank';
+        matchName.innerHTML = match.tooltipInfo.matchName;
+
+        const ratingChange = document.createElement('span');
+        ratingChange.innerHTML = match.ratingChange.toFixed(1);
+        ratingChange.className = clsx(
+          styles.tooltip_ratingChange,
+          match.ratingChange.toFixed(1) > 0
+            ? styles.gain
+            : match.ratingChange.toFixed(1) < 0
+            ? styles.loss
+            : ''
+        );
+
+        li.appendChild(matchName);
+        li.appendChild(ratingChange);
+        matchesList.appendChild(li);
+      });
+
+      const divRoot = tooltipEl.querySelector('div.tooltip');
+
+      // Remove old children
+      while (divRoot.firstChild) {
+        divRoot.firstChild.remove();
+      }
+
+      divRoot.appendChild(matchesList);
+    }
+
+    const { offsetLeft: positionX, offsetTop: positionY } = chart.canvas;
+
+    // Display, position, and set styles for font
+    tooltipEl.style.opacity = 1;
+    /* tooltipEl.style.left = positionX + tooltip.caretX + 'px'; */
+    tooltipEl.style.top = positionY + tooltip.caretY + 'px';
+    tooltipEl.style.font = tooltip.options.bodyFont.string;
+    tooltipEl.style.padding =
+      tooltip.options.padding + 'px ' + tooltip.options.padding + 'px';
+    tooltipEl.style.pointerEvents = 'none';
+
+    var offset = tooltip.width + 120;
+    if (chart.width / 2 < tooltip.caretX) {
+      offset *= -1;
+    } else {
+      offset *= -0.2;
+    }
+
+    // Hidden Code
+    tooltipEl.style.left = positionX + tooltip.caretX + offset + 'px';
+  };
+
   const options = {
     responsive: true,
+    events: ['mousemove', 'click', 'touchstart', 'touchmove'],
     plugins: {
       legend: {
         display: false,
@@ -163,7 +214,7 @@ export default function AreaChart({
       tooltip: {
         enabled: false,
         position: 'nearest',
-        /* external: externalTooltipHandler, */
+        external: ratingStats ? externalTooltipHandler : null,
       },
       /* title: {
         display: true,
@@ -176,7 +227,7 @@ export default function AreaChart({
       },
       point: {
         radius: 0 /* 0 makes points hidden */,
-        hitRadius: 0,
+        hitRadius: 100,
       },
     },
     maintainAspectRatio: false,
@@ -201,41 +252,6 @@ export default function AreaChart({
         },
       },
     },
-  };
-
-  const dateFormatOptions = { year: 'numeric', month: 'short', day: 'numeric' };
-
-  let labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  let dataForGraph: number[] = labels.map(() => Math.ceil(Math.random() * 100));
-
-  if (ratingStats) {
-    labels = ratingStats.map((match) =>
-      new Date(match.tooltipInfo.matchDate).toLocaleDateString(
-        'en-US',
-        dateFormatOptions
-      )
-    );
-    dataForGraph = ratingStats.map((match) => match.ratingAfter.toFixed(0));
-  }
-
-  /* if (rankChart) {
-    labels = rankChart.map((match) => match.matchName);
-    dataForGraph = rankChart.map((match) => match.rank.toFixed(0));
-  } */
-
-  const data = {
-    labels,
-    datasets: [
-      {
-        fill: true,
-        label: '',
-        data: dataForGraph,
-        borderWidth: 3,
-        borderColor: `hsla(${colors[0]}, 0.6)`,
-        backgroundColor: 'transparent' /* `hsla(${colors[1]}, 0.6)` */,
-        font: font,
-      },
-    ],
   };
 
   return (
