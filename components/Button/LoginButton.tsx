6effa5b4be9cae906a54e277615e0b7b39d0fea9
Giovanni Martire <akinari@outlook.it>
2024-01-31 23:44:25 +0100

    Hide login button if user is logged

--- components/Button/LoginButton.tsx
+++ components/Button/LoginButton.tsx
@@ -1,26 +1,30 @@
 'use client';
 import { loginIntoWebsite } from '@/app/actions';
+import { useUser } from '@/util/hooks';
 
 export default function LoginButton() {
-  return (
-    <button
-      onClick={(e) => {
-        e.preventDefault();
-        return loginIntoWebsite();
-      }}
-      style={{
-        padding: '1.2rem 5rem',
-        backgroundColor: 'hsla(var(--blue-500))',
-        border: '0',
-        borderRadius: 'var(--main-borderRadius)',
-        fontSize: '1.2rem',
-        fontWeight: 500,
-        color: '#fff',
-        width: 'fit-content',
-        cursor: 'pointer',
-      }}
-    >
-      Login
-    </button>
-  );
+  const user = useUser();
+
+  if (!user?.osuId)
+    return (
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          return loginIntoWebsite();
+        }}
+        style={{
+          padding: '1.2rem 5rem',
+          backgroundColor: 'hsla(var(--blue-500))',
+          border: '0',
+          borderRadius: 'var(--main-borderRadius)',
+          fontSize: '1.2rem',
+          fontWeight: 500,
+          color: '#fff',
+          width: 'fit-content',
+          cursor: 'pointer',
+        }}
+      >
+        Login
+      </button>
+    );
 }
