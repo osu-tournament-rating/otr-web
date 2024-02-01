aa4d378fdb85ac570f97d16695d51a9d18831803
Giovanni Martire <akinari@outlook.it>
2024-02-01 11:41:58 +0100

    Added missing metadata to pages

--- app/submit/page.tsx
+++ app/submit/page.tsx
@@ -3,6 +3,12 @@ import MatchForm from '@/components/SubmitMatches/MatchForm/MatchForm';
 import { getUserData } from '../actions';
 import styles from './page.module.css';
 
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Submit',
+};
+
 export default async function page() {
   const { roles } = await getUserData();
 
