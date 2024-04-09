18b398cb8c94ba2c00cbca8dff6e44a7ff2acd86
Giovanni Martire <akinari@outlook.it>
2024-04-09 19:19:11 +0200

    fix dark mode user profile and homepage logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,16 @@
-'use client';
-
 import LoginButton from '@/components/Button/LoginButton';
+import Logo from '@/components/Homepage/Logo/Logo';
 import decoration1 from '@/public/decorations/decoration-1.svg';
 import decoration2 from '@/public/decorations/decoration-2.svg';
 import decoration3 from '@/public/decorations/decoration-3.svg';
 import decoration4 from '@/public/decorations/decoration-4.svg';
 import fullLogoDark from '@/public/logos/full-logo-dark.svg';
 import fullLogo from '@/public/logos/full-logo.svg';
-import { useTheme } from 'next-themes';
 import Image from 'next/image';
 import Balancer from 'react-wrap-balancer';
 import styles from './page.module.css';
 
 export default function Home() {
-  const { theme } = useTheme();
-
   return (
     <main className={styles.main}>
       <div className={styles.row}>
@@ -25,13 +21,7 @@ export default function Home() {
           <LoginButton />
         </div>
         <div className={styles.box}>
-          <div className={styles.logo}>
-            <Image
-              src={theme === 'light' ? fullLogo : fullLogoDark}
-              alt={'o!TR logo'}
-              fill
-            />
-          </div>
+          <Logo />
         </div>
       </div>
       <div className={styles.row}>
