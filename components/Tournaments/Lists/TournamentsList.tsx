435a8cd07410de3ce33abd388363688508ff60c2
Giovanni Martire <akinari@outlook.it>
2024-08-11 11:43:44 +0200

    basic sorting

--- components/Tournaments/Lists/TournamentsList.tsx
+++ components/Tournaments/Lists/TournamentsList.tsx
@@ -2,10 +2,11 @@
 
 import Pagination from '@/components/Pagination/Pagination';
 import { modeIcons } from '@/lib/types';
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import { Tooltip } from 'react-tooltip';
 import styles from './Lists.module.css';
 
@@ -19,21 +20,73 @@ export default function TournamentsList({
   //! to remove, just to avoid massive data on the page
   data.length = 5;
 
+  const [sorting, setSorting] = useState({ name: 'name', direction: 'desc' });
+
+  const changeSorting = (sortName: string) => {
+    setSorting(({ name, direction }) => ({
+      name: sortName,
+      direction:
+        sortName === name ? (direction === 'desc' ? 'asc' : 'desc') : 'desc',
+    }));
+  };
+
+  useEffect(() => {
+    if (sorting) {
+      data.sort((a, b) => {
+        if (a[sorting.name] < b[sorting.name]) {
+          return sorting.direction === 'asc' ? 1 : -1;
+        } else if (a[sorting.name] > b[sorting.name]) {
+          return sorting.direction === 'asc' ? -1 : 1;
+        }
+
+        return 0;
+      });
+    }
+  }, [data, sorting]);
+
   return (
     <div>
       <table className={styles.table}>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Format</th>
-            <th>Ruleset</th>
-            <th>
-              Date <FontAwesomeIcon icon={faAngleDown} />
+            <th onClick={() => changeSorting('name')}>
+              Name
+              {sorting.name === 'name' && (
+                <FontAwesomeIcon
+                  icon={sorting.direction === 'asc' ? faAngleUp : faAngleDown}
+                />
+              )}
+            </th>
+            <th onClick={() => changeSorting('teamSize')}>
+              Format
+              {sorting.name === 'teamSize' && (
+                <FontAwesomeIcon
+                  icon={sorting.direction === 'asc' ? faAngleUp : faAngleDown}
+                />
+              )}
+            </th>
+            <th onClick={() => changeSorting('mode')}>
+              Ruleset
+              {sorting.name === 'mode' && (
+                <FontAwesomeIcon
+                  icon={sorting.direction === 'asc' ? faAngleUp : faAngleDown}
+                />
+              )}
+            </th>
+            <th onClick={() => changeSorting('date')}>
+              Date
+              {sorting.name === 'date' && (
+                <FontAwesomeIcon
+                  icon={sorting.direction === 'asc' ? faAngleUp : faAngleDown}
+                />
+              )}
             </th>
           </tr>
         </thead>
         <tbody>
           {data.map((tournament, index) => {
+            const format = `${tournament.teamSize}v${tournament.teamSize}`;
+
             return (
               <tr key={index}>
                 <td>
@@ -41,7 +94,7 @@ export default function TournamentsList({
                     {tournament.name}
                   </Link>
                 </td>
-                <td>Missing format</td>
+                <td>{format}</td>
                 <td>
                   <div className={styles.rulesetIcon}>
                     <Tooltip
