7113f326bc2771f8bdff523d105704bd82429bb2
Giovanni Martire <akinari@outlook.it>
2024-08-14 17:51:45 +0200

    header title change

--- app/matches/[id]/page.tsx
+++ app/matches/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { fetchMatchPage } from '@/app/actions';
 import InfoContainer from '@/components/Tournaments/InfoContainer/InfoContainer';
 import { dateFormatOptions } from '@/lib/types';
+import linkIcon from '@/public/icons/out.svg';
+import Image from 'next/image';
 import styles from './page.module.css';
 
 export default async function page({
@@ -10,11 +12,20 @@ export default async function page({
 }) {
   const matchData = await fetchMatchPage(id);
 
+  console.log(matchData);
+
   return (
     <main className={styles.container}>
       <div className={styles.content}>
         <div className={styles.header}>
-          <h1 className={styles.title}>Missing 6 - 1 Missing</h1>
+          <h1 className={styles.title}>
+            <span>Missing</span>
+            <span className={styles.score}>6 - 1</span>
+            <span>Missing</span>
+            {/* <span className={styles.icon}>
+              <Image src={linkIcon} alt="link icon" fill />
+            </span> */}
+          </h1>
           <div className={styles.date}>
             {new Date(matchData?.startTime).toLocaleDateString(
               'en-US',
