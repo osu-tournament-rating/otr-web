1b3676d3932bf7ba3a060a5f76b773bc794c3f48
Placni <pat.lacni@yahoo.com>
2024-12-02 01:57:33 -0600

    fix key violation

--- components/Form/InputError/InputError.tsx
+++ components/Form/InputError/InputError.tsx
@@ -9,7 +9,7 @@ export default function FormInputError({ message }: { message: string | string[]
   
   return (
     <span className={styles.inputError}>
-      {Array.isArray(message) ? message.map(m => (<p>{m}</p>)) : (<p>{message}</p>)}
+      {Array.isArray(message) ? message.map((m, idx) => (<p key={`error${idx}`}>{m}</p>)) : (<p>{message}</p>)}
     </span>
   )
 }
