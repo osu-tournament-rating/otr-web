3f524e61ab619d1a089f91f20627260f4bd34855
Placni <pat.lacni@yahoo.com>
2024-12-08 01:26:16 -0600

    text for match rejection and warning flag enums

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,8 @@
+// noinspection SpellCheckingInspection
+
 import {
   HttpValidationProblemDetails,
+  MatchRejectionReason, MatchWarningFlags,
   ProblemDetails,
   Roles,
   Ruleset
@@ -37,7 +40,7 @@ export function isHttpValidationProblemDetails(obj: any): obj is HttpValidationP
   );
 }
 
-/** Denotes if a list of scopes containes the admin scope */
+/** Denotes if a list of scopes contains the admin scope */
 export function isAdmin(scopes: string[]) {
   return scopes.includes(Roles.Admin);
 }
@@ -85,3 +88,73 @@ export const rulesetIcons: { [key in Ruleset]: RulesetIconContent } = {
     shortAlt: 'Mania 7K'
   }
 };
+
+/** Text for displaying {@link MatchRejectionReason} */
+export const matchRejectionReasonText: {
+  [key in MatchRejectionReason]: {
+    text: string;
+    description: string;
+  }
+} = {
+  [MatchRejectionReason.None]: {
+    text: 'None',
+    description: 'No description'
+  },
+  [MatchRejectionReason.NoData]: {
+    text: 'No data',
+    description: 'The osu! API returned invalid or no data for the match'
+  },
+  [MatchRejectionReason.NoGames]: {
+    text: 'No games',
+    description: 'The osu! API returned no games for the match'
+  },
+  [MatchRejectionReason.NamePrefixMismatch]: {
+    text: 'Prefix mismatch',
+    description: 'The match\'s name does not start with the tournament\'s abbreviation'
+  },
+  [MatchRejectionReason.FailedTeamVsConversion]: {
+    text: 'Failed TeamVs Conversion',
+    description: 'The match was eligible for TeamVs Conversion, but the conversion was not successful'
+  },
+  [MatchRejectionReason.NoValidGames]: {
+    text: 'No valid games',
+    description: 'None of the match\'s games passed automation checks with a status of Pre-Verified'
+  },
+  [MatchRejectionReason.UnexpectedGameCount]: {
+    text: 'Unexpected game count',
+    description: 'The match has less than 3 Pre-Verified or Verified games'
+  },
+  [MatchRejectionReason.NoEndTime]: {
+    text: 'No end time',
+    description: 'The match\'s end time could not be determined'
+  },
+  [MatchRejectionReason.RejectedTournament]: {
+    text: 'Rejected tournament',
+    description: 'The match\'s tournament was rejected'
+  }
+}
+
+/** Text for displaying {@link MatchWarningFlags} */
+export const matchWarningFlagText: {
+  [key in MatchWarningFlags]: {
+    text: string;
+    description: string;
+  }
+} = {
+  [MatchWarningFlags.None]: {
+    text: 'None',
+    description: 'No description'
+  },
+  [MatchWarningFlags.UnexpectedNameFormat]: {
+    text: 'Unexpected name format',
+    description: 'The match\'s name does not follow expected title formatting'
+  },
+  [MatchWarningFlags.LowGameCount]: {
+    text: 'Low game count',
+    description: 'The match has exactly 4 or 5 Pre-Verified or Verified games'
+  },
+  [MatchWarningFlags.UnexpectedBeatmapsFound]: {
+    text: 'Unexpected beatmaps found',
+    description: 'The match has one or more games where a beatmap that was not pooled was played outside of the first two games'
+  },
+}
