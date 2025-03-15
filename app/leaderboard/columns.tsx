c120ca8ba78653cc2c82f3eb5881cdc9a6921c20
hburn7 <hburnett777@gmail.com>
2025-03-15 14:55:38 -0400

    Leaderboard

--- app/leaderboard/columns.tsx
+++ app/leaderboard/columns.tsx
@@ -16,14 +16,20 @@ export const columns = [
   columnHelper.accessor('player.osuId', {
     header: 'Player',
     cell: ({ getValue, row }) => (
-      <div className="flex flex-row gap-2">
-        <Image
-          src={`https://assets.ppy.sh/old-flags/${row.original.player.country}.png`}
-          alt="avatar"
-          className="p-1"
-          width={40}
-          height={28}
-        />
+      <div className="flex flex-row gap-2 font-sans">
+        <div className="flex flex-row gap-1">
+          <p className="self-end align-bottom text-xs text-secondary-foreground">
+            #{row.original.countryRank}
+          </p>
+          <Image
+            src={`https://assets.ppy.sh/old-flags/${row.original.player.country}.png`}
+            alt="avatar"
+            className="p-1"
+            width={40}
+            height={28}
+          />
+        </div>
+
         <Image
           src={`https://a.ppy.sh/${getValue()}`}
           alt="avatar"
@@ -41,6 +47,30 @@ export const columns = [
     header: 'Tier',
     cell: ({ getValue }) => (
       <TierIcon tier={getValue() ?? ''} width={24} height={24} />
+    ),
+  }),
+  columnHelper.accessor('rating', {
+    header: 'Rating',
+    cell: ({ getValue }) => <p className="font-sans">{~~getValue()}</p>,
+  }),
+  columnHelper.accessor('tournamentsPlayed', {
+    header: 'Tournaments',
+    cell: ({ getValue }) => (
+      <p className="font-sans text-secondary-foreground">{getValue()}</p>
+    ),
+  }),
+  columnHelper.accessor('matchesPlayed', {
+    header: 'Matches',
+    cell: ({ getValue }) => (
+      <p className="font-sans text-secondary-foreground">{getValue()}</p>
+    ),
+  }),
+  columnHelper.accessor('winRate', {
+    header: 'Win %',
+    cell: ({ getValue }) => (
+      <p className="font-sans text-secondary-foreground">
+        {~~((getValue() ?? 0) * 100)}%
+      </p>
     ),
   }),
 ];
