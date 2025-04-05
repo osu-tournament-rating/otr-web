042b888bfc572e8cf4e3defe33bfb9ea41358068
hburn7 <hburnett777@gmail.com>
2025-04-05 12:08:55 -0400

    style: add cursor-pointer and adjust mobile dropdown alignment

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -193,7 +193,7 @@ export default function Header() {
                           <button
                             onClick={() => toggleMobileDropdown(item.title)}
                             className={cn(
-                              'flex w-full justify-between bg-secondary px-4 py-2 text-lg transition-colors hover:bg-secondary hover:text-primary focus:bg-secondary',
+                              'flex w-full justify-between bg-secondary px-4 py-2 text-lg transition-colors hover:bg-secondary hover:text-primary hover:cursor-pointer focus:bg-secondary',
                               pathname.startsWith(item.href) &&
                                 'font-extrabold text-primary focus:text-primary'
                             )}
@@ -214,13 +214,13 @@ export default function Header() {
                                   key={dropdownItem.title}
                                   asChild
                                   className={cn(
-                                    'flex w-full items-center gap-2 bg-secondary px-4 py-2 pl-8 text-lg transition-colors hover:bg-secondary hover:text-primary focus:bg-secondary focus:text-foreground',
+                                    'flex w-full items-center justify-start gap-2 bg-secondary px-4 py-2 pl-8 text-lg transition-colors hover:bg-secondary hover:text-primary hover:cursor-pointer focus:bg-secondary focus:text-foreground',
                                     pathname === dropdownItem.href &&
                                       'font-medium text-primary focus:text-primary'
                                   )}
                                 >
                                   <Link href={dropdownItem.href}>
-                                    <div className="flex items-center gap-2">
+                                    <div className="flex items-center justify-start gap-2">
                                       <dropdownItem.icon className="h-4 w-4" />
                                       {dropdownItem.title}
                                     </div>
