de0b5779352eb9fb133927a72f4474c945be41f8
hburn7 <hburnett777@gmail.com>
2025-04-05 14:12:02 -0400

    fix: Handle undefined warning flag metadata with error message

--- components/badges/WarningFlagsBadge.tsx
+++ components/badges/WarningFlagsBadge.tsx
@@ -6,6 +6,7 @@ import { ApiItemType } from '@/lib/types';
 import { TriangleAlertIcon } from 'lucide-react';
 import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip';
 import { Badge } from '../ui/badge';
+import { toast } from 'sonner';
 
 export default function WarningFlagsBadge({
   itemType,
@@ -23,14 +24,21 @@ export default function WarningFlagsBadge({
       ? MatchWarningFlagsEnumHelper.getMetadata(value)
       : GameWarningFlagsEnumHelper.getMetadata(value);
 
+  if (metadata.includes(undefined)) {
+    metadata.push({
+      text: `[BUG]: Received unexpected warning flag value. Metadata is undefined for flag ${value} on type ${itemType}`,
+      description: '',
+    });
+  }
+
   return (
     <Tooltip>
       <TooltipContent>
         <div>
           <strong>Warnings:</strong>
-          <ul className="list-disc pl-3.5 mt-1">
+          <ul className="mt-1 list-disc pl-3.5">
             {metadata.map(({ text }, index) => (
-                <li key={index}>{text}</li>
+              <li key={index}>{text}</li>
             ))}
           </ul>
         </div>
