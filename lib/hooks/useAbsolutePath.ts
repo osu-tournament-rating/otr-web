1c3efae86ae56409ae6f8978d42e883b2ab1be6d
hburn7 <hburnett777@gmail.com>
2025-05-18 16:29:52 -0400

    Document useAbsolutePath

--- lib/hooks/useAbsolutePath.ts
+++ lib/hooks/useAbsolutePath.ts
@@ -2,6 +2,11 @@
 
 import { usePathname } from 'next/navigation';
 
+/**
+ * A client component hook for reading
+ * the current page's absolute URL path
+ * @returns Current page's absolute URL path
+ */
 export function useAbsolutePath() {
   const path = usePathname();
 
