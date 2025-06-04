76567b505cd0b57a114302dd0abcac5b5ed27b2d
hburn7 <hburnett777@gmail.com>
2025-06-03 20:49:21 -0400

    Fix build issue

--- components/player/PlayerRatingStatsCard.tsx
+++ components/player/PlayerRatingStatsCard.tsx
@@ -36,7 +36,10 @@ export default function PlayerRatingStatsCard({
 }: PlayerRatingStatsCardProps) {
   return (
     <Card className="p-6 font-sans">
-      <PlayerCard player={rating.player} />
+      <PlayerCard
+        player={rating.player}
+        provisional={rating.isProvisional ?? false}
+      />
       <div className="flex flex-col gap-4">
         {/* Stats Cards */}
         <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
