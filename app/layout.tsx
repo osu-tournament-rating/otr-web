2e86d2d6fe65e67b82d4693a56395bc35071c5a4
hburn7 <hburnett777@gmail.com>
2025-06-04 18:16:35 -0400

    Font hotfix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,23 +2,12 @@ import Header from '@/components/header/Header';
 import { ThemeProvider } from 'next-themes';
 import { Toaster } from '@/components/ui/sonner';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import React from 'react';
 import './globals.css';
 import Footer from '@/components/footer/Footer';
 import SessionProvider from '@/components/session-provider';
 import { getSession } from '@/lib/api/server';
 import { TooltipProvider } from '@/components/ui/tooltip';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: {
@@ -37,9 +26,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`font-sans ${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="font-inter">
         <ThemeProvider attribute="class" disableTransitionOnChange>
           <TooltipProvider>
             <SessionProvider user={session}>
