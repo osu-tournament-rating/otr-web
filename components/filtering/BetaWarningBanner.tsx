7b189f6ed401fe7c45aad2beea6a560618f26489
hburn7 <hburnett777@gmail.com>
2025-06-29 12:55:11 -0400

    Prettier

--- components/filtering/BetaWarningBanner.tsx
+++ components/filtering/BetaWarningBanner.tsx
@@ -6,8 +6,8 @@ export function BetaWarningBanner() {
     <Alert className="border-yellow-500 bg-yellow-50 dark:bg-yellow-950/20 [&>svg]:text-yellow-600 [&>svg]:dark:text-yellow-500">
       <AlertTriangle className="h-4 w-4" />
       <AlertDescription className="text-yellow-800 dark:text-yellow-200">
-        This tool is available for beta testing only. It is not ready for use
-        in officially-supported tournaments.
+        This tool is available for beta testing only. It is not ready for use in
+        officially-supported tournaments.
       </AlertDescription>
     </Alert>
   );
