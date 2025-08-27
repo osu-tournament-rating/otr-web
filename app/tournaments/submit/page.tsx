ea1de41a9defcc12dbfa0a80ce63dab3a98570d4
hburn7 <hburnett777@gmail.com>
2025-08-26 20:23:40 -0400

    feat: better tournament submission page layout

--- app/tournaments/submit/page.tsx
+++ app/tournaments/submit/page.tsx
@@ -1,5 +1,4 @@
 import TournamentSubmissionForm from '@/components/tournaments/TournamentSubmissionForm';
-import { Card } from '@/components/ui/card';
 import { ExternalLink, Trophy } from 'lucide-react';
 import { Metadata } from 'next';
 import Link from 'next/link';
@@ -11,27 +10,25 @@ export const metadata: Metadata = {
 
 export default async function TournamentSubmissionPage() {
   return (
-    <div className="container mx-auto flex flex-col gap-4 px-4 md:gap-2 md:px-0">
-      <Card className="flex flex-col items-center justify-center gap-3 px-4 py-6 sm:gap-4 sm:px-6">
-        <div className="flex flex-row items-center gap-2 text-lg font-bold text-primary sm:text-xl md:text-2xl">
-          <Trophy className="size-7 sm:size-8 md:size-9" />
+    <div className="container mx-auto px-4 py-8">
+      <div className="mb-8 text-center">
+        <h1 className="flex items-center justify-center gap-2 text-2xl font-bold text-primary">
+          <Trophy className="size-8" />
           Tournament Submission
-        </div>
-        <div className="flex flex-col items-center text-center">
-          <span className="text-sm text-muted-foreground sm:text-base">
-            Use this form to submit a new tournament for verification and
-            tracking.
-          </span>
-          <Link
-            href="https://docs.otr.stagec.xyz/Rating-Framework/Data-Processing/Tournament-Approval#acceptance-criteria"
-            target="_blank"
-            className="mt-1 flex items-center text-sm text-primary underline underline-offset-4 transition-colors hover:text-primary/80 sm:text-base"
-          >
-            <ExternalLink className="mr-1 size-3.5 sm:size-4" />
-            Read our acceptance criteria
-          </Link>
-        </div>
-      </Card>
+        </h1>
+        <p className="mt-4 text-muted-foreground">
+          Use this form to submit a new tournament for verification and
+          tracking.
+        </p>
+        <Link
+          href="https://docs.otr.stagec.xyz/Rating-Framework/Data-Processing/Tournament-Approval#acceptance-criteria"
+          target="_blank"
+          className="mt-3 inline-flex items-center text-sm text-primary underline underline-offset-4 transition-colors hover:text-primary/80"
+        >
+          <ExternalLink className="mr-1 size-4" />
+          Read our acceptance criteria
+        </Link>
+      </div>
       <TournamentSubmissionForm />
     </div>
   );
