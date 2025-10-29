72df80a47cc3ad42de5f95407b7fa8cf7b244f02
SourMongoose <chrisxiongyang@yahoo.com>
2025-10-29 14:14:21 -0700

    Treat spunout as nomod (#609)

--- apps/web/components/player/PlayerModStatsChart.tsx
+++ apps/web/components/player/PlayerModStatsChart.tsx
@@ -64,11 +64,12 @@ export default function PlayerModStatsChart({
       );
 
       const metadata = ModsEnumHelper.getMetadata(stat.mods);
-      // Join mod texts and remove all "NF" occurrences
+      // Join mod texts and remove all "NF" and "SO" occurrences
       let label = metadata
         .map((meta) => meta.text)
         .join('')
-        .replace(/NF/g, '');
+        .replace(/NF/g, '')
+        .replace(/SO/g, '');
 
       // If label is empty, it's "No Mod" (NM)
       if (label === '') {
