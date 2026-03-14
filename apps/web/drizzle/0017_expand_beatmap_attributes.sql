612985261d57163d2abd6fc5b18485df5aff0453
myssto <pat.lacni@yahoo.com>
2026-03-14 02:15:33 -0500

    Manually remove index statements from migration

--- apps/web/drizzle/0017_expand_beatmap_attributes.sql
+++ apps/web/drizzle/0017_expand_beatmap_attributes.sql
@@ -5,7 +5,3 @@ ALTER TABLE "beatmap_attributes" ADD COLUMN "cs" double precision NOT NULL;--> s
 ALTER TABLE "beatmap_attributes" ADD COLUMN "bpm" double precision NOT NULL;--> statement-breakpoint
 ALTER TABLE "beatmap_attributes" ADD COLUMN "total_length" bigint NOT NULL;--> statement-breakpoint
 ALTER TABLE "beatmap_attributes" ADD COLUMN "drain_length" integer NOT NULL;--> statement-breakpoint
-CREATE INDEX "ix_game_audits_changes_gin" ON "game_audits" USING gin ("changes" jsonb_path_ops);--> statement-breakpoint
-CREATE INDEX "ix_game_score_audits_changes_gin" ON "game_score_audits" USING gin ("changes" jsonb_path_ops);--> statement-breakpoint
-CREATE INDEX "ix_match_audits_changes_gin" ON "match_audits" USING gin ("changes" jsonb_path_ops);--> statement-breakpoint
-CREATE INDEX "ix_tournament_audits_changes_gin" ON "tournament_audits" USING gin ("changes" jsonb_path_ops);
