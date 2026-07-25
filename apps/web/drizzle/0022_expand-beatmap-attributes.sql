d5b288cc502fa8f95961bef9242759543e19b168
myssto <pat.lacni@yahoo.com>
2026-07-24 23:40:12 -0500

    Re-generate old migration

--- apps/web/drizzle/0022_expand-beatmap-attributes.sql
+++ apps/web/drizzle/0022_expand-beatmap-attributes.sql
@@ -4,4 +4,4 @@ ALTER TABLE "beatmap_attributes" ADD COLUMN "hp" double precision NOT NULL;--> s
 ALTER TABLE "beatmap_attributes" ADD COLUMN "cs" double precision NOT NULL;--> statement-breakpoint
 ALTER TABLE "beatmap_attributes" ADD COLUMN "bpm" double precision NOT NULL;--> statement-breakpoint
 ALTER TABLE "beatmap_attributes" ADD COLUMN "total_length" bigint NOT NULL;--> statement-breakpoint
-ALTER TABLE "beatmap_attributes" ADD COLUMN "drain_length" integer NOT NULL;--> statement-breakpoint
+ALTER TABLE "beatmap_attributes" ADD COLUMN "drain_length" integer NOT NULL;
